e3970b0a6468ec2966bb8b37b4143078c072e3f6
pherphepheni <osorio.f@outlook.es>
2020-04-22 14:39:19 -0300

    Updates

---
+++ precache-manifest.ce2cf47e9a9fd35310cc4deb2a1f6fcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011298de6c95cf5fd0e3c75161102b66",
+    "url": "/mpintegracion/index.html"
+  },
+  {
+    "revision": "df0240d59fea8de56232",
+    "url": "/mpintegracion/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b015dce0158509aa2677",
+    "url": "/mpintegracion/static/js/2.f1607ba8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mpintegracion/static/js/2.f1607ba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df0240d59fea8de56232",
+    "url": "/mpintegracion/static/js/main.57ad7ac2.chunk.js"
+  },
+  {
+    "revision": "d00e8344b183b32aff62",
+    "url": "/mpintegracion/static/js/runtime-main.cb76ea5c.js"
+  }
+]);
